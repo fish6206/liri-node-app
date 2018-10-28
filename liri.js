ade16ba6e4bf2300bb4bc254806ac66ede3a7981
Greg Fish <fish.greg4@gmail.com>
2018-10-27 21:03:00 -0700

    Update liri.js

--- liri.js
+++ liri.js
@@ -1,10 +1,23 @@
 
 require("dotenv").config();
+//initiate spotify API
+//require spotify API
+var SpotifyWebApi = require('spotify-web-api-node');
+//spotify ID and info
+var spotifyApi = new SpotifyWebApi({
+  clientId: 'da40305d6bf84ba9997d3887b394eae4',
+  clientSecret: 'a0b86b8dcc4845cf8045db637519f751',
+ redirectUri: 'http://www.example.com/callback'
+});
+// Include the request npm package for OMDB (Don't forget to run "npm install request" in this folder first!)
+var request = require("request");
+//reuire bandsintown API
+//var bandsintown = require('bandsintown')(APP_ID);
 
 //link to keys.js
 var keys = require("keys.js");
 //variable for spotify keys
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 //grab command line argument
 var search = process.argv[2];
 // Joining the remaining arguments since an actor or tv show name may contain spaces
@@ -14,28 +27,27 @@ var term = process.argv.slice(3).join(" ");
 
 //print if searching spotify
 if (search === "spotify") {
-  console.log("Searching Spotify for song");
-  spotify.findSong(term);
+  console.log("Searching Spotify for song: " + term);
 
   //spotify search from site, added term variable for query
-  spotify.search({ type: 'track', query: term }, function (err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-
-    console.log(data);
+  spotifyApi.searchTracks(term)
+  .then(function(data) {
+    console.log('Search by: ' + term, data.body);
+  }, function(err) {
+    console.error(err);
   });
 }
 
+
 //print if searching bands in town
 if (search === "concert") {
-  console.log("Searching for band in town");
+  console.log("Searching for band in town: " + term);
   //create bands constructor NOT SURE IF THIS WORKS
   var BANDS = function() {
     //findBand takes in name of band and searches bands in town API
     this.findBand =function(band) {
-      //IS BAND THE RIGHT VARIABLE OR IS IT TERM?
-      var URL = "https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp";
+    
+      var URL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=653b62a76c013cf37ddb69e12b666503";
       // parse body (string) to a JSON object
       var jsonData = JSON.parse(body);
 
@@ -45,11 +57,27 @@ if (search === "concert") {
 
       ].join("\n\n");
     }
+    console.log(bandData);
   }
 }
 
 //print if searching OMDb
 if (search === "movie") {
-  console.log("Searching fo movie");
+  console.log("Searching for movie: " + term);
+  // Then run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy";
 
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
+
+  request(queryUrl, function (error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      console.log("Release Year: " + JSON.parse(body).Year, "\nIMDB Rating: " + JSON.parse(body).Rated, "\nPlot: " + JSON.parse(body).Plot );
+    }
+  });
 }
